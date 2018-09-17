c51227cad981c1177005e86c273692d92b363deb
johnwayneairobot <johnwayneairobot@gmail.com>
2018-09-16 22:37:05 -0400

    2018.09.16 10:36PMEST try again set safety all output

--- panda/board/safety/safety_kia.h
+++ panda/board/safety/safety_kia.h
@@ -1,48 +1,32 @@
-// board enforces
-//   in-state
-//      accel set/resume
-//   out-state
-//      cancel button
-//      accel rising edge
-//      brake rising edge
-//      brake > 0mph
+void default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {}
 
-// these are set in the Honda safety hooks...this is the wrong place
-//const int gas_interceptor_threshold = 328;
-//int gas_interceptor_detected = 1;
-//int brake_prev = 0;
-//int gas_prev = 0;
-//int gas_interceptor_prev = 0;
-//int ego_speed = 0;
+int default_ign_hook() {
+  return -1; // use GPIO to determine ignition
+}
 
-// 2018.09.16 12:59PMEST copy from subaru to enable all
-
-void kia_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {}
-
- //int kia_ign_hook() {
-  // return 1; // use on start up
-// }
-
-// FIXME
 // *** all output safety mode ***
 
-static void kia_init(int16_t param) {
+static void alloutput_init(int16_t param) {
   controls_allowed = 1;
 }
 
-static int kia_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
+static int alloutput_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   return true;
 }
 
-//static int kia_tx_lin_hook(int lin_num, uint8_t *data, int len) {
- // return true;
-//}
+static int alloutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
+  return true;
+}
 
+static int alloutput_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
+  return -1;
+}
 
-const safety_hooks kia_hooks = {
-  .init = kia_init,
-  .rx = kia_rx_hook,
-  .tx = kia_tx_hook,
-  //.tx_lin = kia_tx_lin_hook,
-  // ignition = kia_ign_hook,
+const safety_hooks alloutput_hooks = {
+  .init = alloutput_init,
+  .rx = default_rx_hook,
+  .tx = alloutput_tx_hook,
+  .tx_lin = alloutput_tx_lin_hook,
+  .ignition = default_ign_hook,
+  .fwd = alloutput_fwd_hook,
 };
